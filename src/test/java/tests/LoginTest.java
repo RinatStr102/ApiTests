b959d9c972a24110912f95c8dbdfe0148018cbd9
RinatStr102 <tamam-102@mail.ru>
2024-12-15 23:30:24 +0300

    Исправил тест с авторизацией demoqa

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,24 +3,25 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class LoginTest {
 
     @Test
     void successfulLogin() {
-        String requestBody = "{\"username\": \"IlonMask\", \"password\":\"Password123!\"}";
+        String requestBody = "{\"userName\": \"IlonMask\", \"password\": \"Password123!\"}";
 
-        // Отправим POST
-    given()
-            .baseUri("https://demoqa.com")
-            .basePath("/login")
-            .header("Content-Type", "application/json")
-            .body(requestBody)
-            .when()
-            .post()
-            .then()
-            .statusCode(200)
-            .body("token", equalTo("expected_token"));
+        // Отправим POST-запрос
+        given()
+                .baseUri("https://demoqa.com")
+                .basePath("/Account/v1/Login")
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .when()
+                .post()
+                .then()
+                .statusCode(200) // ожидаем статус 200 OK
+                .body("token", notNullValue()) // проверяем, что токен возвращается
+                .log().all(); // логируем ответ для наглядности
     }
 }
